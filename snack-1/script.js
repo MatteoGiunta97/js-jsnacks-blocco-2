57cf7a545a084698e11cad3551b4016708105372
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-12 14:55:40 +0100

    feat: add snack 1, js

--- snack-1/script.js
+++ snack-1/script.js
@@ -7,4 +7,12 @@
 const userNum = parseInt(prompt('inserire un numero'));
 
 // Se il numero inserito è pari lo stampo
+if (userNum % 2 === 0) {
+    let printNum = userNum;
+    console.log(printNum)
+
 // Se invece il numero è dispari stampo il numero successivo a quello inserito
+}else {
+    printNum = userNum + 1;
+    console.log(printNum)
+}
