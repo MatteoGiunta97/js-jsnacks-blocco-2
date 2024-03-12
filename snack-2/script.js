3526cbc9a7d8cdd993dfd849c4bec921570f365b
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-12 15:29:58 +0100

    feat: add for loops, snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -10,4 +10,16 @@ const firstWord = prompt('inserisci una parola');
 const secondWord = prompt('inserisci una seconda parola');
 
 // Controllo il numero di lettere di ogni parola
+for (let index = 0; index < firstWord.length; index++) {
+    
+    fWordLength = firstWord[index];
+    //console.log(fWordLength)
+}
+
+for (let index = 0; index < secondWord.length; index++) {
+    
+    sWordLength = secondWord[index];
+    //console.log(sWordLength)
+}
+
 // Stampo prima la parole che contiene meno lettere e poi quella con più lettere
