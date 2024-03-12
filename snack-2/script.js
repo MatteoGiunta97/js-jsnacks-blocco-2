9459693cc3304e91377ab30dbcb1df9803c5267b
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-12 15:06:40 +0100

    feat: add comments and prompts, snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,3 +3,11 @@
 Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
+
+
+// Chiedo di inserire due parole
+const firstWord = prompt('inserisci una parola');
+const secondWord = prompt('inserisci una seconda parola');
+
+// Controllo il numero di lettere di ogni parola
+// Stampo prima la parole che contiene meno lettere e poi quella con più lettere
