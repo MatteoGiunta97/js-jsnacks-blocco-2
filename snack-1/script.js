85cb8dd41e662d2308db62b90441c66177c31282
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-12 14:49:05 +0100

    init: snack 1

---
+++ snack-1/script.js
@@ -0,0 +1,10 @@
+// - L'utente inserisce un numero nel prompt, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+
+
+// Chiedo di inserire un numero
+const userNum = parseInt(prompt('inserire un numero'));
+
+// Se il numero inserito è pari lo stampo
+// Se invece il numero è dispari stampo il numero successivo a quello inserito
